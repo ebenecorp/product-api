b5148e0dd458c9f1ff70127fd5f610e7985ecad9
unknown <ebene2009@gmail.com>
2021-05-13 00:55:23 +0100

    prepare authController for registering

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Models\User;
+use Illuminate\Support\Facade\Hash;
+
 
 class AuthController extends Controller
 {
     //
-    
+
+    public function register (RegisterRequest $request){
+
+        
+
+    }
+
 }
